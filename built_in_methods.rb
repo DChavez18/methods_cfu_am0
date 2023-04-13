8c3c4d0c58edf88e68789dc422c4d3c8ba79bb15
Derek Chavez <frrenose17@yahoo.com>
2023-04-13 14:42:37 -0600

    Add content to final practice

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -10,19 +10,27 @@
 
 
 "Hello World".include?("Hello")
+# The include? method is called on the string object "Hello World".
+# True is the return value, .include? is asking whether or not the string provided includes the string "Hello".
 
 
 "Hello World".end_with?("Hello")
+# The .end_with? method is called on the string "Hello World"
+# false is the return value as the .end_with? method is determining if the string "Hello World" ends with the string "Hello".
 
 
 "Hello World".end_with?("rld")
+# The .end_with? method is called on the string "Hello World"
+# The return value is true becuase the .end_with? method is determining if the string "hello World" ends with the string "rld".
 
 
 12.even?
-
+# I called the .even? method on the integer 2
+# The return value was true because the integer 2 is an even number
 
 18.next
-
+# I called the .next method on the integer 2
+# The return value was 3 because the .next method is determining what integer follows the integer 2.
 
 
 # SECTION 2: Calling methods on variables assigned to strings.
@@ -30,6 +38,16 @@
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/String.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+name = "derek".size
+puts name.size
+# I called the method .size on the string "derek"
+# The return value was 5 as the .size method was determing how many letters were in the string "derek".
+
+location = "denver".empty?
+puts location.empty?
+# I called the method .empty? on the string "denver"
+# The return value was false because the "denver" string contains letters
 
 # EXAMPLE
 # The start_with? method is called on the first_name variable, which stores the string object "Jeff". 
@@ -47,6 +65,17 @@ puts first_name.start_with?("J")
 # https://ruby-doc.org/core-3.1.0/Integer.html
 # Include comments above each method call explaining the impact and return value of that method.
 
+age = 32
+puts age.integer?
+# I called the method .integer? on the age variable, which stores the integer 32.
+# The integer? method returns true because 32 is an integer.
+# The puts command prints the return value of the integer? method which was true.
+
+pets = 3
+puts pets.zero?
+# I called the method .zero? on the pets variable, which stores the integer 3.
+# The zero? method returns the value as false, because the integer stored in the pets variable does not equal zero.
+# The puts command prints the return value false to the console.
 
 
 # SECTION 4: Calling methods on variables assigned to arrays.
@@ -54,3 +83,17 @@ puts first_name.start_with?("J")
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+friends = [1, 2, 3]
+puts  friends.drop(3)
+# I called the drop method on the friends variable, which stores an array of integers from 1 to 3.
+# The drop method returned nothing in the output, because the argument 3 was placed next to the drop method it removed
+# all 3 elements in the array.
+# The puts command printed nothing in return as all elements in the array were "dropped"
+
+friends = ["cam", "vic", "nate"]
+puts friends.rotate
+
+# I called the rotate method on the friends variable, which stores an array of strings.
+# The rotate method returned the value ["vic "nate", "cam"] which moved "cam" from position 0 to position 2
+# The puts command printed ["vic "nate", "cam"] as the string "cam" was rotated to the last position in the array.
--- final_practice.rb
+++ final_practice.rb
@@ -4,36 +4,89 @@
 # Use the puts or print command to see the return value in the console.
 
 
-# 1: Write a method named greeting that returns a string with a general greeting. 
+# 1: Write a method named greeting that returns a string with a general greeting.
+
+def greeting
+    "hello!"
+end
+
+p greeting
 
 # What is the return value of your method?
+# "hello!
 # How many arguments did you pass your method?
+# 0 arguments were passed through the method
+
 
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
 
+def greeting(name)
+    "hello #{name}!"
+end
+
+p greeting("derek")
+p greeting("shelby")
+
+
 # What is the return value of your method?
+# "hello derek!"
+# "hello shelby!"
 # How many arguments did you pass your method?
+# 1 arguments were passed through the method
 # What data type was your argument(s)?
+# String
 
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
-# What is the return value of your method?
+def greet_person(first_name, middle_name, last_name)
+     "Hello #{first_name} #{middle_name} #{last_name}!"
+  end
+  
+  p greet_person("derek", "martin", "chavez")
+
+# What is the return value of your method? "Hello derek martin chavez!"
 # How many arguments did you pass your method?
+# - Three arguments were passed through the method
 # What data type was your argument(s)?
+# - Strings
 
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
 
-# What is the return value of your method?
+def square(num)
+    return "The square root of #{num} = #{Math.sqrt(num)}"
+end
+
+p square(4)
+
+# What is the return value of your method? 2.0
 # How many arguments did you pass your method?
+# - 1 argument was passed through my method
 # What data type was your argument(s)?
+# - Integer
 
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
+
+def check_stock(num, str)
+    if num >= 4
+      return "#{str} is stocked"
+    elsif num <= 1
+      return "#{str} -  running low"
+    elsif num == 0 
+      return " #{str} - Out of stock"
+    end
+
+      
+    
+end
+    
+        
+
 
 check_stock(4, "Coffee");
 # => "Coffee is stocked"
@@ -46,3 +99,7 @@ check_stock(0, "Cheese");
 
 check_stock(1, "Salsa");
 # => "Salsa - running LOW"
+
+
+
+
